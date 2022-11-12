6841e965bbe2e773342db38b67529aa6c44ccccc
SotenesMartins <sostenesmartins1996@gmail.com>
2022-11-12 13:02:26 -0300

    add logic in megadasorte

--- app/src/main/java/com/example/numerosdasorte/MegaActivity.kt
+++ app/src/main/java/com/example/numerosdasorte/MegaActivity.kt
@@ -2,10 +2,54 @@ package com.example.numerosdasorte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MegaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mega)
+
+        val editNumber: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerator: Button = findViewById(R.id.btn_generator)
+
+
+        btnGenerator.setOnClickListener {
+            val text = editNumber.text.toString()
+            numberGenerator(text, txtResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Escolha um número entre 6 e 15", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val qtd = text.toInt()
+
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Escolha um número entre 6 e 15", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
     }
 }
